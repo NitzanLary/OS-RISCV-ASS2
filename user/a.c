10a7ad8a1c937e482c97d9179e7fce5118d11380
AsafStern <asafste@gmail.com>
2022-05-08 00:04:23 +0300

    hopefully finished conc ll

--- user/a.c
+++ user/a.c
@@ -1,7 +1,9 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/proc.h"
+// #include "kernel/proc.h"
+
+
 
 
 int
@@ -19,31 +21,28 @@ t1(){
     ////pushpush?
     // if(getpid() == f_pid){
     //     for (int i = 0; i < 64; i++){
-    //         printf("%d  ", pids[i]);
+    //         printf("%d ", pids[i]);
     //     }
-    // }
-    // if (getpid() % 10 == 0)
-    // {
-    //     printf("_");
     // }
     if (getpid() > 60)
     {
         printf("%d ", getpid() % 10);
     }
+    return 0;
 }
 
-int t2(){
-    struct proc_ll q = {-1, -1};
-    int n_forks = 2;
-    for (int i=0; i < n_forks; i++){
-        enque(&q, getpid());
-        fork();
-    }
-}
+// int t2(){
+//     struct proc_ll q = {-1, -1};
+//     int n_forks = 2;
+//     for (int i=0; i < n_forks; i++){
+//         enque(&q, getpid());
+//         fork();
+//     }
+// }
 
 int
 main(int argc, char *argv[])
 {
-    t2();
+    t1();
     exit(0);
 }
